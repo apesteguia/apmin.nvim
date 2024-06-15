0db0ad320dc6b6f1dccf331bea9e9f22295ed1c0
apesteguia <mikeltxomikeltxo@gmail.com>
2024-06-15 23:37:45 +0200

    colors

--- lua/apesteguia/init.lua
+++ lua/apesteguia/init.lua
@@ -102,7 +102,7 @@ local set_groups = function()
 		Keyword = { fg = c.blue, bg = c.none, bold = cfg.keyword_style.bold, italic = cfg.keyword_style.italic }, -- italic for, do, while, etc.
 		Repeat = { fg = c.blue, bg = c.none, bold = cfg.keyword_style.bold, italic = cfg.keyword_style.italic }, -- italic any other keyword
 		Boolean = { fg = c.cyan, bg = c.none, bold = cfg.boolean_style.bold, italic = cfg.boolean_style.italic }, -- true , false
-		Function = { fg = c.blue, bg = c.none, bold = cfg.function_style.bold, italic = cfg.function_style.italic },
+		Function = { fg = c.white, bg = c.none, bold = cfg.function_style.bold, italic = cfg.function_style.italic },
 		Identifier = { fg = c.white, bg = c.none }, -- any variable name
 		String = { fg = c.green, bg = c.none }, -- Any string
 		Character = { fg = c.green }, -- any character constant: 'c', '\n'
